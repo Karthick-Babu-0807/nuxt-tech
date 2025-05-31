d3c4033dda59c5c3c01747d264f12fcae9cc11fc
Karthick-Babu-0807 <118100299+Karthick-Babu-0807@users.noreply.github.com>
2025-06-01 00:43:43 +0530

    [NT]-feat: Adding tailwind css and daisy UI (#2)

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <span>Hello there! </span>
+    <Icon name="ph:hand-peace-duotone" />
   </div>
 </template>
